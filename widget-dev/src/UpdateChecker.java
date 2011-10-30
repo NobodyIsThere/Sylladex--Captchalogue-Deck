5427184e28a348f23876700414e0b8f0912bbdc3
Alexander McCabe <mccabea@earthlink.net>
2011-10-30 02:31:24 -0400

    Finished update checker: added images, instructions, preferences, maybe other stuff I forgot.

--- widget-dev/src/UpdateChecker.java
+++ widget-dev/src/UpdateChecker.java
@@ -1,3 +1,4 @@
+import java.awt.Component;
 import java.awt.Desktop;
 import java.io.IOException;
 import java.io.InputStream;
@@ -5,12 +6,22 @@ import java.net.MalformedURLException;
 import java.net.URI;
 import java.net.URISyntaxException;
 import java.net.UnknownHostException;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 import java.util.Timer;
 import java.util.TimerTask;
 import java.util.concurrent.TimeUnit;
+import javax.swing.Box;
+import javax.swing.BoxLayout;
+import javax.swing.Icon;
+import javax.swing.ImageIcon;
+import javax.swing.JLabel;
 import javax.swing.JOptionPane;
+import javax.swing.JSpinner;
+import javax.swing.SpinnerNumberModel;
 import javax.swing.SwingUtilities;
+import sylladex.Main;
 import sylladex.Widget;
 
 public class UpdateChecker extends Widget
@@ -24,7 +35,7 @@ public class UpdateChecker extends Widget
 				URI rss = null, website = null; //temp variables that aren't final for the try block
 				try
 					{
-						rss = new URI(/*"http"*/"file", /*"//www.mspaintadventures.com/rss/*/"///C:/Users/Alexander/Sylladex--Captchalogue-Deck/program-dev/"+"rss.xml", null);
+						rss = new URI("http", "//www.mspaintadventures.com/rss/rss.xml", null);
 						website = new URI("http", "//www.mspaintadventures.com/", null);
 					}
 				catch(URISyntaxException e)
@@ -36,9 +47,20 @@ public class UpdateChecker extends Widget
 			}
 		/** The default interval at which to check for updates (5 minutes). */
 		private static final int DEFAULT_CHECK_INTERVAL = 300;
-		public enum State
+		public static enum State
 			{
-				UPDATE, NO_UPDATE, NO_CONNECTION, ERROR
+				UPDATE, NO_UPDATE, NO_CONNECTION, ERROR;
+				public static final String folder = "widgets/UpdateChecker/";
+				private Icon dockIcon = Main.createImageIcon(folder + "doc_" + toString() + ".gif");
+				private ImageIcon cardIcon = Main.createImageIcon(folder + "card_" + toString() + ".gif");
+				public Icon getDockIcon()
+					{
+						return dockIcon;
+					}
+				public ImageIcon getCardIcon()
+					{
+						return cardIcon;
+					}
 			}
 
 		/**
@@ -49,6 +71,15 @@ public class UpdateChecker extends Widget
 			{
 				// see run()
 				private boolean justRead = false;
+
+				/** Icon used when a check is done. */
+				public Icon dockCheckIcon = Main.createImageIcon(State.folder + "doc_check.gif");
+				public ImageIcon cardCheckIcon = Main.createImageIcon(State.folder + "card_check.gif");
+
+				/*
+				 * Note: card icons are sized to fit the default card size, since they're
+				 * animations, and resizing doesn't seem to work on them.
+				 */
 
 				/** number after "s=" for the latest known page */
 				private int adventure;
@@ -96,6 +127,14 @@ public class UpdateChecker extends Widget
 
 				private Status check()
 					{
+						SwingUtilities.invokeLater(new Runnable(){
+							@Override
+							public void run()
+								{
+									//check icon takes 600 ms to play
+									changeIcon(dockCheckIcon, cardCheckIcon, 600);
+								}
+						});
 						try
 							{
 								InputStream rssStream = null;
@@ -126,6 +165,7 @@ public class UpdateChecker extends Widget
 										return new Status(State.NO_UPDATE); // :(
 									}
 								//I think this indicates there isn't an internet connection?
+								//I found it from a Google search
 								//It works for me, at least
 								catch(UnknownHostException e)
 									{
@@ -159,8 +199,6 @@ public class UpdateChecker extends Widget
 						 * check independent of the widget. Also, it would be nice to be
 						 * able to just set the update as read without opening the
 						 * browser, for the same reason.
-						 */
-						/*
 						 * When the user goes to MSPA after an update is detected, I want
 						 * to resume checking for further updates. However, it is possible
 						 * that there's been an update between when the update was
@@ -191,24 +229,73 @@ public class UpdateChecker extends Widget
 
 		// timer thread is daemon: want to run as long as the sylladex is open but no longer
 		private Timer timer = new Timer(true);
+		/**
+		 * Icon can't be changed yet, because the check icon is in the process of running.
+		 */
+		private boolean iconBlocked = false;
+		/** JPanel holding the icon for the card. */
+		private JLabel card_icon = new JLabel();
 
-		//below variables should all be set in load(), but are initialized just in case
-		/** number of seconds between checks */
-		private int checkInterval = DEFAULT_CHECK_INTERVAL;
+		//below variables should all be set in load()/prepare()
+		/** number of seconds between checks. */
+		private int checkInterval;
 		/** current state of the <code>UpdateChecker</code> */
-		private State state = null;
+		private State state;
 		/** If an exception occurs, goes into ERROR state and stores the exception here */
-		private Throwable error = null;
+		private Throwable error;
 		/** the most recent Check to check the site */
-		private Check check = null;
+		private Check check;
+
+		/**
+		 * Sets the dock icon to <code>dockIcon</code>. Waits <code>animTimeMillis</code>
+		 * milliseconds, blocking the icon from being changed again, then updates the icon
+		 * to match the state, if <code>animTimeMillis > 0</code>. Otherwise, just leaves
+		 * the icon as the given icon.
+		 */
+		private void changeIcon(Icon dockIcon, ImageIcon cardIcon, long animTimeMillis)
+			{
+				if(iconBlocked)
+					return; //icon blocked; don't change it
+
+				dock_icon.setIcon(dockIcon);
+				card_icon.setIcon(cardIcon);
+
+				//it seems to work without refreshing/repainting the dock
+				//so i'll leave this commented out.
+				//refreshDoc() also seems to show the dock, and I don't want it to be popping up every time a check is run
+				//(^ also, I just noticed that I forgot the k in refreshDock(). I think I'll leave it. :P)
+				//m.refreshDock();
+				//m.refreshCardHolder();
+				if(animTimeMillis <= 0)
+					return;
+				iconBlocked = true;
+				timer.schedule(new TimerTask(){
+					@Override
+					public void run()
+						{
+							//not in event thread
+							SwingUtilities.invokeLater(new Runnable(){
+								@Override
+								public void run()
+									{
+										iconBlocked = false;
+										updateIcon();
+									}
+							});
+						}
+				}, animTimeMillis);
+			}
+		/** Updates the icon to that of the current state. */
+		private void updateIcon()
+			{
+				changeIcon(state.getDockIcon(), state.getCardIcon(), 0);
+			}
 
 		public void setState(State state)
 			{
 				this.state = state;
-				dock_icon.setText(state.toString());
-				panel.add(dock_icon);
+				updateIcon();
 				System.out.println("UpdateChecker: state = " + state.toString());
-				m.refreshDock();
 			}
 		public void setError(Throwable e)
 			{
@@ -219,7 +306,20 @@ public class UpdateChecker extends Widget
 				setState(State.ERROR);
 				setError(e);
 			}
+		/** Sets the check interval and reschedules the check. */
+		public void setCheckInterval(int interval)
+			{
+				checkInterval = interval;
+				check.cancel();
+				scheduleCheck(check.getAdventure(), check.getLastPage(), false);
+			}
 
+		/**
+		 * @param adventure the number of the current adventure
+		 * @param lastPage the number of the last page read
+		 * @param justRead if <code>true</code>, uses the first check just to get
+		 *        up-to-date (see {@link Check#run()})
+		 */
 		private Check scheduleCheck(int adventure, int lastPage, boolean justRead)
 			{
 				Check check = new Check(adventure, lastPage, justRead);
@@ -227,11 +327,20 @@ public class UpdateChecker extends Widget
 				return check;
 			}
 
+		/** Initialization; called by both prepare() and load(). */
+		private void init()
+			{
+				panel.setOpaque(false);
+				panel.add(card_icon);
+			}
+
 		@Override
 		public void prepare()
 			{
-				System.out.println("prepare");
-				error(new IllegalStateException("UpdateChecker was not initialized."));
+				checkInterval = DEFAULT_CHECK_INTERVAL;
+				setState(State.NO_UPDATE);
+				check = scheduleCheck(0, 0, true); //use first check to get up-to-date
+				init();
 			}
 		@Override
 		public void load(String string)
@@ -257,24 +366,56 @@ public class UpdateChecker extends Widget
 						return;
 					}
 				if("u".equals(vars[0]))
-					setState(State.UPDATE); //don't need to check if I already know there's an unread update
+					{
+						setState(State.UPDATE); //don't need to check if I already know there's an unread update
+						//make a Check but don't run it; just to keep track of adventure/last page
+						check = new Check(adventure, lastPage, true);
+					}
 				else
-					check = scheduleCheck(adventure, lastPage, false);
+					{
+						setState(State.NO_UPDATE);
+						check = scheduleCheck(adventure, lastPage, false);
+					}
+				init();
 			}
 
 		/**
-		 * If the state is UPDATE or NO_UPDATE, opens MSPA in the default browser. If the
-		 * state, is NO_CONNECTION or ERROR, outputs the error.
+		 * Open a dialog allow user to set preferences. Current preferences: <list>
+		 * <item>Check interval: time between successive checks.</item> </list>
+		 */
+		//TODO: atm, only opens if NO_UPDATE. would be nice to be able to open whenever.
+		private void preferencesDialog()
+			{
+				//components to update each preference
+				List<Component> preferences = new ArrayList<Component>();
+				//component for interval; holds label & spinner
+				Box interval = new Box(BoxLayout.X_AXIS);
+				preferences.add(interval);
+				/*
+				 * min: 0 (each check comes immediately after the last)
+				 * max: 2^31-1 (don't want to overflow)
+				 * step: 1 second (don't think you really need to be more precise)
+				 */
+				SpinnerNumberModel intervalSpinner = new SpinnerNumberModel(checkInterval, 0, Integer.MAX_VALUE, 1);
+				interval.add(new JLabel("Check Interval (seconds):"));
+				interval.add(new JSpinner(intervalSpinner));
+				JOptionPane.showMessageDialog(null, preferences.toArray(), "Set Update Checker Preferences", JOptionPane.PLAIN_MESSAGE);
+				setCheckInterval(intervalSpinner.getNumber().intValue());
+			}
+
+		/**
+		 * If the state is UPDATE opens MSPA in the default browser. If the state is
+		 * NO_UPDATE, opens the preferences dialog. If the state, is NO_CONNECTION or
+		 * ERROR, outputs the error.
 		 */
 		@Override
 		public void open()
 			{
 				switch(state)
 					{
-						case UPDATE:
+						case UPDATE: {
 							setState(State.NO_UPDATE);
 							check = scheduleCheck(check.getAdventure(), check.getLastPage(), true);
-						case NO_UPDATE: {
 							if(Desktop.isDesktopSupported())
 								{
 									Desktop desktop = Desktop.getDesktop();
@@ -293,6 +434,9 @@ public class UpdateChecker extends Widget
 								}
 							JOptionPane.showMessageDialog(null, "Unable to open browser.", "Browse Not Supported", JOptionPane.ERROR_MESSAGE);
 						}
+						break;
+						case NO_UPDATE:
+							preferencesDialog();
 						break;
 						case NO_CONNECTION:
 						case ERROR:
